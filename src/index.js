9befd70031d80d793d2269508d94496b5c005a49
anthonyreis <matheus.anthony1@gmail.com>
2021-08-11 00:53:42 -0300

    adicao de rotas de task

--- src/index.js
+++ src/index.js
@@ -19,6 +19,25 @@ app.post('/users', (req, res) => {
 	});
 });
 
+app.get('/users', (req, res) => {
+	User.find({}).then((users) => {
+		res.status(200).send(users);
+	}).catch((err) => {
+		res.status(500).send({ err: err.message });
+	});
+});
+
+app.get('/users/:id', (req, res) => {
+	User.findById(req.params.id).then((user) => {
+		if (!user) {
+			res.status(404).send({ msg: 'User not found' });
+		}
+		res.status(200).send(user);
+	}).catch((err) => {
+		res.status(500).send({ err: err.message });
+	});
+});
+
 app.post('/tasks', (req, res) => {
 	const task = new Task(req.body);
 
@@ -26,6 +45,25 @@ app.post('/tasks', (req, res) => {
 		res.status(201).send(task);
 	}).catch((err) => {
 		res.status(400).send({ err: err.message });
+	});
+});
+
+app.get('/tasks', (req, res) => {
+	Task.find({}).then((tasks) => {
+		res.status(200).send(tasks);
+	}).catch((err) => {
+		res.status(500).send({ err: err.message });
+	});
+});
+
+app.get('/tasks/:id', (req, res) => {
+	Task.findById(req.params.id).then((task) => {
+		if (!task) {
+			res.status(404).send({ msg: 'Task not found' });
+		}
+		res.status(200).send(task);
+	}).catch((err) => {
+		res.status(500).send({ err: err.message });
 	});
 });
 
