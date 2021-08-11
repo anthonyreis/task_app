b6e8894876b4eb17e1137f3c82c794cff85ae1de
anthonyreis <matheus.anthony1@gmail.com>
2021-08-11 11:58:05 -0300

    adicao de rotas patch

--- src/index.js
+++ src/index.js
@@ -45,6 +45,28 @@ app.get('/users/:id', async (req, res) => {
 	}
 });
 
+app.patch('/users/:id', async (req, res) => {
+	const updates = Object.keys(req.body);
+	const allowedUpdates = ['name', 'email', 'password', 'age'];
+	const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+	if (!isValidOperation) {
+		res.status(400).send({ err: 'Invalid updates' });
+	}
+
+	try {
+		const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+
+		if (!user) {
+			res.status(404).send({ msg: 'User not found' });
+		}
+
+		res.status(200).send(user);
+	} catch (e) {
+		res.status(400).send({ err: e.message });
+	}
+});
+
 app.post('/tasks', async (req, res) => {
 	try {
 		const task = new Task(req.body);
@@ -70,6 +92,28 @@ app.get('/tasks', async (req, res) => {
 app.get('/tasks/:id', async (req, res) => {
 	try {
 		const task = await Task.findById(req.params.id);
+
+		if (!task) {
+			res.status(404).send({ msg: 'Task not found' });
+		}
+
+		res.status(200).send(task);
+	} catch (e) {
+		res.status(500).send({ err: e.message });
+	}
+});
+
+app.patch('/tasks/:id', async (req, res) => {
+	const update = Object.keys(req.body);
+	const allowedUpdates = ['description', 'completed'];
+	const isValidOperation = update.every((update) => allowedUpdates.includes(update));
+
+	if (!isValidOperation) {
+		res.status(400).send({ err: 'Invalid Updates' });
+	}
+
+	try {
+		const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
 
 		if (!task) {
 			res.status(404).send({ msg: 'Task not found' });
